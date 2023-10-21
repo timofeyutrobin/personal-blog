d6cfd00acf55ab23c6aecb4b39480d489e1af7e5
Timofey Utrobin <->
2023-10-22 00:55:05 +0500

    music volume slider and mute btn

---
+++ src/routes/music/UnmuteIcon.svelte
@@ -0,0 +1,14 @@
+<svg viewBox="0 0 32 32" role="graphics-symbol" focusable="false">
+    <title>Unmute</title>
+    <path
+        fill="currentColor"
+        d="M21,15.242l2-2V27l-8.598-5.159l1.458-1.458L21,23.468V15.242z M27.707,5.707l-22,22
+	l-1.414-1.414L9.586,21H8V11h5l10-6v2.586l3.293-3.293L27.707,5.707z M21,9.586V8.532L13.554,13H10v6h1.586L21,9.586z"
+    />
+</svg>
+
+<style lang="scss">
+    svg {
+        color: $text-color-primary;
+    }
+</style>
