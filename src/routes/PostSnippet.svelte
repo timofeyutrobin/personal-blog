8e576f0f74e500bc6dcfb25827f169329884c3a8
Timofey Utrobin <33760130+timofeyutrobin@users.noreply.github.com>
2025-01-13 03:50:12 +0500

    comment removed

--- src/routes/PostSnippet.svelte
+++ src/routes/PostSnippet.svelte
@@ -19,36 +19,3 @@
     <h2 class="text-3xl"><a href="/post/{id}">{title}</a></h2>
     <p>{description}</p>
 </article>
-
-<!-- <style lang="scss">
-    article {
-        margin-bottom: indent(3);
-        @include sheet;
-    }
-
-    h2 {
-        margin-bottom: indent(1);
-        @include heading-large;
-    }
-
-    p {
-        font-size: 1.2rem;
-        margin-top: indent(1);
-    }
-
-    img {
-        width: 100%;
-        height: 500px;
-        object-fit: contain;
-        object-position: left top;
-    }
-
-    @include mobile {
-        img {
-            height: auto;
-        }
-        article {
-            margin-bottom: indent(5);
-        }
-    }
-</style> -->
