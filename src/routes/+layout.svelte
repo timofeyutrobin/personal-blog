2771619ab21713eaa5a3d30aecc42035d0a4100a
Timofey Utrobin <33760130+timofeyutrobin@users.noreply.github.com>
2025-01-30 23:59:29 +0500

    dark theme in localStorage

--- src/routes/+layout.svelte
+++ src/routes/+layout.svelte
@@ -1,7 +1,23 @@
 <script lang="ts">
     import '../app.css';
+    import localStorage from '$lib/localStorage';
     import Header from './Header.svelte';
+    import { setContext } from 'svelte';
+
+    let { children } = $props();
+
+    let isDarkTheme = $state(false);
+    setContext('toggleDarkTheme', () => (isDarkTheme = !isDarkTheme));
+
+    $effect(() => {
+        isDarkTheme = localStorage.getItem('theme') === 'dark';
+    });
+
+    $effect(() => {
+        localStorage.setItem('theme', isDarkTheme ? 'dark' : 'light');
+        document.documentElement.classList.toggle('dark', isDarkTheme);
+    });
 </script>
 
 <Header />
-<slot />
+{@render children()}
--- src/routes/ThemeButton.svelte
+++ src/routes/ThemeButton.svelte
@@ -1,8 +1,11 @@
 <script lang="ts">
+    import { getContext } from 'svelte';
     import ThemeIcon from './icons/ThemeIcon.svelte';
     let { class: className }: { class?: string } = $props();
+
+    let onclick = getContext<() => void>('toggleDarkTheme');
 </script>
 
-<button class={className} onclick={() => document.documentElement.classList.toggle('dark')}>
+<button class={className} {onclick}>
     <ThemeIcon class="fill-current" />
 </button>
