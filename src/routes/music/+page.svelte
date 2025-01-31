f5a0c33c22f9f3c0ffc9d252f13b56a53ca04ba1
Timofey Utrobin <->
2025-02-01 02:43:22 +0500

    music page loading

--- src/routes/music/+page.svelte
+++ src/routes/music/+page.svelte
@@ -9,7 +9,14 @@
     const trackList = Object.keys(tracks) as TrackId[];
 
     let audioElements = $state<Record<string, HTMLMediaElement>>({});
-    let audioDuration = $state<Record<string, number>>({});
+    let isAudioLoaded = $state(
+        Object.keys(tracks).reduce<Record<string, boolean>>((result, trackId) => {
+            result[trackId] = false;
+            return result;
+        }, {})
+    );
+    let isLoading = $derived(Object.values(isAudioLoaded).some((isLoaded) => !isLoaded));
+
     let volume = $state(1);
     $effect(() => {
         Object.values(audioElements).forEach((audio) => (audio.volume = volume));
@@ -82,66 +89,77 @@
     <title>Timofey Utrobin - music</title>
 </svelte:head>
 
+{#each trackList as trackId}
+    <audio
+        bind:this={audioElements[trackId]}
+        id="audio-{trackId}"
+        src={tracks[trackId].src}
+        {onended}
+        {ontimeupdate}
+        onloadeddata={() => (isAudioLoaded[trackId] = true)}
+    ></audio>
+{/each}
 <main
     class="
-        container relative
-        mx-auto mb-8 max-w-full p-6
-        shadow shadow-zinc-300
-        sm:my-4 md:w-9/12 xl:w-7/12
-        dark:shadow-none dark:bg-zinc-800
+    container relative
+    mx-auto mb-8 max-w-full p-6
+    shadow shadow-zinc-300
+    sm:my-4 md:w-9/12 xl:w-7/12
+    dark:shadow-none dark:bg-zinc-800
     "
 >
-    <h1 class="mb-2 text-3xl">My music</h1>
-    <div class="absolute right-6 top-6 flex items-center">
-        <button
-            role="switch"
-            aria-label="mute"
-            aria-checked={isMuted}
-            class="
-                h-8 w-8 mr-2
-                rounded-full p-1 outline-none transition-colors
-                hover:bg-indigo-50 focus:bg-indigo-50 active:bg-indigo-50
-                dark:hover:bg-indigo-800 dark:focus:bg-indigo-800 dark:active:bg-indigo-800"
-            onclick={() => {
-                if (isMuted) {
-                    volume = volumeBeforeMute;
-                } else {
-                    volumeBeforeMute = volume;
-                    volume = 0;
-                }
-            }}
-        >
-            {#if isMuted}
-                <UnmuteIcon class="fill-current" />
-            {:else}
-                <MuteIcon class="fill-current" />
-            {/if}
-        </button>
-        <Slider class="hidden w-[150px] sm:block" step={0.05} min={0} max={1} bind:value={volume} />
-    </div>
-    <div class="flex flex-col items-center sm:block">
-        {#each trackList as trackId}
-            <audio
-                bind:this={audioElements[trackId]}
-                id="audio-{trackId}"
-                src={tracks[trackId].src}
-                {onended}
-                {ontimeupdate}
-                onloadedmetadata={(event) =>
-                    (audioDuration[trackId] = (event.target as HTMLAudioElement).duration)}
-            ></audio>
-            <Track
-                id={trackId}
-                title={tracks[trackId].title}
-                cover={tracks[trackId].cover}
-                isCurrent={trackId === currentTrackId}
-                {isPaused}
-                {onplay}
-                totalTime={audioDuration[trackId]}
-                {currentTime}
-                onseek={(time) => (audioElements[trackId].currentTime = time)}
-                peaks={tracks[trackId].peaks}
+    {#if isLoading}
+        LOADING...
+    {:else}
+        <h1 class="mb-2 text-3xl">My music</h1>
+        <div class="absolute right-6 top-6 flex items-center">
+            <button
+                role="switch"
+                aria-label="mute"
+                aria-checked={isMuted}
+                class="
+            h-8 w-8 mr-2
+            rounded-full p-1 outline-none transition-colors
+            hover:bg-indigo-50 focus:bg-indigo-50 active:bg-indigo-50
+            dark:hover:bg-indigo-800 dark:focus:bg-indigo-800 dark:active:bg-indigo-800"
+                onclick={() => {
+                    if (isMuted) {
+                        volume = volumeBeforeMute;
+                    } else {
+                        volumeBeforeMute = volume;
+                        volume = 0;
+                    }
+                }}
+            >
+                {#if isMuted}
+                    <UnmuteIcon class="fill-current" />
+                {:else}
+                    <MuteIcon class="fill-current" />
+                {/if}
+            </button>
+            <Slider
+                class="hidden w-[150px] sm:block"
+                step={0.05}
+                min={0}
+                max={1}
+                bind:value={volume}
             />
-        {/each}
-    </div>
+        </div>
+        <div class="flex flex-col items-center sm:block">
+            {#each trackList as trackId}
+                <Track
+                    id={trackId}
+                    title={tracks[trackId].title}
+                    cover={tracks[trackId].cover}
+                    isCurrent={trackId === currentTrackId}
+                    {isPaused}
+                    {onplay}
+                    totalTime={audioElements[trackId].duration}
+                    {currentTime}
+                    onseek={(time) => (audioElements[trackId].currentTime = time)}
+                    peaks={tracks[trackId].peaks}
+                />
+            {/each}
+        </div>
+    {/if}
 </main>
