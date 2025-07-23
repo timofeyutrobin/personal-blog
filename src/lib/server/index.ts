2d51cc996c7bb45c96fbee9875e9b50ac1a26314
Timofey Utrobin <33760130+timofeyutrobin@users.noreply.github.com>
2025-07-24 01:42:40 +0500

    rename env vars

--- src/lib/server/index.ts
+++ src/lib/server/index.ts
@@ -1,10 +1,10 @@
-import { POSTGRES_URL } from '$env/static/private';
+import { NEON_DATABASE_URL } from '$env/static/private';
 import { Sequelize } from 'sequelize-typescript';
 import pg from 'pg';
 import { Post } from '$lib/server/models/Post';
 import { Cover } from '$lib/server/models/Cover';
 
-export const sequelize = new Sequelize(POSTGRES_URL, {
+export const sequelize = new Sequelize(NEON_DATABASE_URL, {
     dialect: 'postgres',
     dialectModule: pg,
     models: [Post, Cover]
